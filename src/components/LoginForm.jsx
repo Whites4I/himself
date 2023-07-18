1a23fb558a10177d0ad6f89da65368774a1b5cc4
Whites4I <andriypetrina2000@gmail.com>
2023-07-18 22:21:34 +0300

    fix

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import Input from "./UI/input/Input";
 import Button from "./UI/button/Button";
-import "./style/LoginForm.module.css";
+import cl from "./style/LoginForm.module.css";
+import { Link } from "react-router-dom";
 
 const LoginForm = () => {
   return (
-    <div>
-      <div className='container'>
-        <form>
-          <Input placeholder='Email@example.com'></Input>
-          <Input placeholder='Password'></Input>
-          <Button>Login</Button>
-        </form>
-      </div>
+    <div className={cl.login}>
+      <form>
+        <div className={cl.hello}>
+          <h1>Hello</h1>
+        </div>
+        <Input placeholder='Email@example.com'></Input>
+        <Input placeholder='Password'></Input>
+        <Button>Login</Button>
+        <Link>Registred</Link>
+      </form>
     </div>
   );
 };
